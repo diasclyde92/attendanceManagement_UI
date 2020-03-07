28c1ce4c4a37399f61910379075c3f35530979f2
Shreya Shirodkar <shirodkarshreya11@gmail.com>
2020-03-07 12:36:25 +0530

    add Login page and Portal page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
+import LoginPage from "./Pages/LoginPage";
+import "./App.css";
+import PortalPage from "./Components/PortalPage";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Route path="/" exact component={LoginPage} />
+          <Route path="/Portal" component={PortalPage} />
+        </Router>
       </div>
     );
   }
