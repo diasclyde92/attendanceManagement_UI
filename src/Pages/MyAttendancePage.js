447c737bf994f7fd1cb77d77bdac47e41b7fb71a
Shreya Shirodkar <shirodkarshreya11@gmail.com>
2020-03-07 13:10:09 +0530

    add attendance page

---
+++ src/Pages/MyAttendancePage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Header from "../Components/Header";
+
+export default class MyAttendancePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container h-100">
+          <div className="row h-100" style={{ marginTop: "1em" }}>
+            <div className="col col-md-6">
+              <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Punch In Time</th>
+                    <th scope="col">Punch Out Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
