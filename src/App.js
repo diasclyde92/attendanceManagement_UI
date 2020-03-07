c903c7b0e99ccf9f112a04be8a12f24c0f72712c
RMartires <rohitmartires14@gmail.com>
2020-03-07 15:24:47 +0530

    added the add user page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import PortalPage from "./Pages/PortalPage";
 import MyAttendancePage from "./Pages/MyAttendancePage"
 import PunchPage from './Pages/Punch';
 import ManageEmployePage from './Pages/ManageEmployePage';
+import AddEmployePage from "./Pages/AddEmployePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
           <Route path="/AttendancePage" component={MyAttendancePage} />
           <Route path="/Punch" component={PunchPage} />
           <Route path="/ManageEmployes" component={ManageEmployePage} />
+          <Route path='/AddEmploye' component={AddEmployePage} />
         </Router>
       </div>
     );
