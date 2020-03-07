e9950c11b74bdc09b92adc89cccba3884d0cd14a
Nathaniel Nunes <manutdkid77@gmail.com>
2020-03-07 16:15:03 +0530

    corrected routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "shards-ui/dist/css/shards.min.css";
 import LoginPage from "./Pages/LoginPage";
 import "./App.css";
 import PortalPage from "./Pages/PortalPage";
-import MyAttendancePage from "./Pages/MyAttendancePage"
-import PunchPage from './Pages/Punch';
-import ManageEmployePage from './Pages/ManageEmployePage';
+import MyAttendancePage from "./Pages/MyAttendancePage";
+import PunchPage from "./Pages/Punch";
+import ManageEmployePage from "./Pages/ManageEmployePage";
 import AddEmployePage from "./Pages/AddEmployePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -15,12 +15,12 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <Route path="/" exact component={PortalPage} />
+          <Route path="/" exact component={PunchPage} />
           <Route path="/Login" component={LoginPage} />
+          <Route path="/Portal" component={PortalPage} />
           <Route path="/AttendancePage" component={MyAttendancePage} />
-          <Route path="/Punch" component={PunchPage} />
           <Route path="/ManageEmployes" component={ManageEmployePage} />
-          <Route path='/AddEmploye' component={AddEmployePage} />
+          <Route path="/AddEmploye" component={AddEmployePage} />
         </Router>
       </div>
     );
