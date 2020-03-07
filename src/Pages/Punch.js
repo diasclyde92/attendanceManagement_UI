6f3acf474b5203d86e4a230b16294a74a666a266
Nathaniel Nunes <manutdkid77@gmail.com>
2020-03-07 15:10:34 +0530

    add pucnh in api call

--- src/Pages/Punch.js
+++ src/Pages/Punch.js
@@ -1,53 +1,81 @@
 import React, { Component } from "react";
-import { Button } from "shards-react";
-import { FormInput } from "shards-react";
+import { FormInput, Button, Alert } from "shards-react";
+import axios from "axios";
 
 class Punch extends Component {
   state = {
-
+    punchValue: 0,
+    showSuccess: false
   };
 
-  componentDidMount() {
+  componentDidMount() {}
 
-  }
-
-  handelchange = (event) => {
+  handelchange = event => {
     this.setState({ value: event.target.value });
-  }
+  };
 
-  submit = () => {
-    console.log(this.state.value);
-    fetch('', {
-      method: 'POST',
-      body: JSON.stringify({ userId: this.state.value })
-    })
-      .then()
-      .catch(err => {
-        console.log(err);
-      });
-
-  }
+  submit = async punchType => {
+    debugger;
+    let attendanceObject = {
+      publicId: localStorage.getItem("publicId"),
+      punchType: punchType
+    };
+    await axios
+      .post("127.0.0.1:5000/api/v1/attendance-records/", attendanceObject)
+      .then(response => {
+        this.setState({
+          punchValue: 1,
+          showSuccess:true
+        });
+      })
+      .catch(error => {});
+  };
 
   render() {
     return (
       <div class="container h-100">
         <div class="row h-100 justify-content-center align-items-center">
           <div className="col col-md-6">
-            <FormInput size="lg" placeholder="UserID" value={this.state.value} onChange={this.handelchange} />
+            <FormInput
+              size="lg"
+              placeholder="UserID"
+              value={this.state.value}
+              onChange={this.handelchange}
+            />
 
-            <Button size="lg" block pill theme="success" style={{ marginTop: '20px' }} onClick={this.submit}>
+            <Button
+              size="lg"
+              block
+              pill
+              theme="success"
+              style={{ marginTop: "20px" }}
+              onClick={() => this.submit("PUNCH_IN")}
+            >
               PunchIN
             </Button>
-            <Button size="lg" block pill theme="secondary" onClick={this.submit}>
+            <Button
+              size="lg"
+              block
+              pill
+              theme="secondary"
+              onClick={() => this.submit("PUNCH_OUT")}
+            >
               PunchOUT
             </Button>
+            <br />
+            <Alert
+              theme="success"
+              dismissible={() => this.setState({ showSuccess: false })}
+              open={this.state.showSuccess}
+            >
+              You have successfully{" "}
+              {this.state.punchType === 1 ? "Punched In" : "Punched Out"}
+            </Alert>
           </div>
         </div>
       </div>
-
     );
   }
 }
-
 
 export default Punch;
